2804c2ff73f734d1d2edf159f1a078df194d6e61
jhona <jhona@192.168.56.1>
2020-03-12 18:20:33 -0300

    PrimeFaces funcionando adicionando e lsitando

---
+++ PUB/src/br/com/pub/controller/ProdutoController.java
@@ -0,0 +1,50 @@
+package br.com.pub.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import br.com.pub.jpaUtil.GenericDAO;
+import br.com.pub.model.Produto;
+
+@SuppressWarnings("deprecation")
+@ManagedBean(name ="ProdutoBean")
+@SessionScoped
+public class ProdutoController implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	Produto produto = new Produto();
+	GenericDAO<Produto> produtoDAO = new GenericDAO<Produto>();
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	public void limparDados(){
+		produto = new Produto();
+	}
+	public String addProduto(){
+		produtoDAO.novo(produto);
+		FacesContext.getCurrentInstance().addMessage(null,new FacesMessage("Salvo com sucesso"));
+		
+		FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+		limparDados();
+		return "listProdutos";
+		
+	}
+	public List<Produto> listarProdutos(){
+		//pegar cabeçalho do datatable e colocar.
+		return produtoDAO.listarTodos(Produto.class);
+	}
+	
+	public String delProduto(){
+		produtoDAO.deletar(produto);
+		return "sucesso";
+	}
+	
+}
